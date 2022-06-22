361d4f24cf787f6622ccc593108c5954313c7d69
Anto426 <amanocchio426@gmail.com>
2022-06-23 01:25:01 +0200

    v3.2

--- events/interaction/bottonandcommand/interactionCreatecommand.js
+++ events/interaction/bottonandcommand/interactionCreatecommand.js
@@ -4,23 +4,29 @@ module.exports = {
 
         if (interaction.isCommand()){
 
+            let owner = false
+            for(let id in configs.owner){
+                if(interaction.member.id ==configs.owner[id]){owner=true}
+            }
+            let staf = false
+            for(let role in configs[interaction.guild.name].role.staff){
+                if(interaction.member.roles.cache.has(configs[interaction.guild.name].role.staff[role])){staf=true}
+            }
+            for(let id in configs.owner){
+                if(interaction.member.id ==configs.owner[id]){staf=true}
+            }
+
         const command = client.commands.get(interaction.commandName)
         if (!command) return
         if(command.onlyStaff){
-            let permesso = false
-            for(let role in configs[interaction.guild.name].role.staff){
-                if(interaction.member.roles.cache.has(configs[interaction.guild.name].role.staff[role])){permesso=true}
-            }
-            for(let id in configs.owner){
-                if(interaction.member.id ==configs.owner[id]){permesso=true}
-            }
-            if(permesso == false ){
+           
+            if(staf == false ){
                 
                 console.log("permesso negato")
                 const embed = new Discord.MessageEmbed()
                 .setTitle("Error")
                 .setDescription(` Non hai i permessi necessari`)
-                .setThumbnail(configs.embed.images.error)
+                .setThumbnail(configs.embed.images.accesdenied)
                 .setColor(configs.embed.color.red)
             interaction.reply({ embeds: [embed], ephemeral: true })
                 
@@ -33,17 +39,14 @@ module.exports = {
         }
 
         if(command.onlyOwner){
-            let permesso = false
-            for(let id in configs.owner){
-                if(interaction.member.id ==configs.owner[id]){permesso=true}
-            }
-            if(permesso == false ){
+
+            if(owner == false ){
                 
                 console.log("permesso negato")
                 const embed = new Discord.MessageEmbed()
                 .setTitle("Error")
                 .setDescription(` Non hai i permessi necessari`)
-                .setThumbnail(configs.embed.images.error)
+                .setThumbnail(configs.embed.images.accesdenied)
                 .setColor(configs.embed.color.red)
             interaction.reply({ embeds: [embed], ephemeral: true })
                 
@@ -53,10 +56,27 @@ module.exports = {
             }else{
                 console.log("permesso accordato")
             }
-        }
-        command.execute(interaction)
-    }
 
+
+           
+        }
+        if(interaction.channel.name == "「💻」comandi" || owner){
+            command.execute(interaction)
+            return
+        }else{
+            console.log("permesso negato")
+            const embed = new Discord.MessageEmbed()
+            .setTitle("Error")
+            .setDescription(`Non hai i permessi necessari  per eseguire i comandi qui !`)
+            .setThumbnail(configs.embed.images.accesdenied)
+            .setColor(configs.embed.color.red)
+        interaction.reply({ embeds: [embed], ephemeral: true })
+        return
+        }
+
+
+    }
+        command.execute(interaction)
 
 
     }
