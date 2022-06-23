4b6804367e02fe4384e8f1c63f1300ef6f261902
Anto426 <amanocchio426@gmail.com>
2022-06-23 18:16:32 +0200

    v3.2

--- commands/server/clear.js
+++ commands/server/clear.js
@@ -22,20 +22,28 @@
                  .setDescription('Non puoi cancellare più di 100 messaggi')
              return interaction.reply({ embeds: [embed] })
          }
-         interaction.channel.bulkDelete(count).catch(err => {
+         try {
+             interaction.channel.bulkDelete(count).then((message) => {
+                 let embed = new Discord.MessageEmbed()
+                     .setColor(configs.embed.color.green)
+                     .setTitle('Messagi cancellati')
+                     .setDescription('Messagi cancellati')
+                     .addField("Numero", `\`\`\`js\n ${message.size}\`\`\``, true)
+                 interaction.reply({ embeds: [embed] }).then(() => {
+                     interaction.deleteReply()
+                 })
+
+             })
+         } catch (err) {
              let embed = new Discord.MessageEmbed()
-                 .setColor(configs.embed.images.color.red)
+                 .setColor(configs.embed.color.red)
                  .setTitle('Error')
-                 .setDescription('Non puoi cancellare i messagi più vecchi di 14 d')
+                 .setDescription('Qualcosa è andato storto')
+                 .setThumbnail(configs.embed.images.error)
              return interaction.reply({ embeds: [embed] })
-         })
-         let embed = new Discord.MessageEmbed()
-             .setColor(configs.embed.color.green)
-             .setTitle('Messagi cancellati')
-             .setDescription('Messagi cancellati')
-             .addField("Numero", `\`\`\`js\n ${count}\`\`\``, true)
-         interaction.reply({ embeds: [embed] })
-         interaction.deleteReply()
+         }
+
+
      }
 
  }
