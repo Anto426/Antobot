85f40500e540cc480b37d7ec38530e146fc0b4ed
Anto426 <amanocchio426@gmail.com>
2022-07-08 12:30:28 +0200

    v3,2

--- commands/general/serverinfo.js
+++ commands/general/serverinfo.js
@@ -6,21 +6,46 @@ module.exports = {
         name: "serverinfo",
         description: "Informazioni sul server"
     },
-    execute(interaction) {
+    async execute(interaction) {
         var server = interaction.member.guild;
+        let countingonline = await server.members.cache.filter(x => x.presence?.status == 'online').size
+        let countingidle = await server.members.cache.filter(x => x.presence?.status == 'idle').size
 
         const embed = new Discord.MessageEmbed()
             .setTitle(server.name)
+            .setColor(configs.embed.color.green)
             .setDescription("Tutte le info su questo server")
             .setThumbnail(server.iconURL({ dynamic: true }))
-            .addField("Owner", `\`\`\`js\n ${server.members.cache.get(server.ownerId).nickname} \`\`\``, true)
-            .addField("Livello di verifica", `\`\`\`js\n Alto \`\`\``, true)
-            .addField("Server id", `\`\`\`js\n ${server.id} \`\`\``, true)
-            .addField("Members", `\`\`\`js\n Membri tot:${server.memberCount.toString()}||Bot:${server.members.cache.filter(x => x.user.bot).size.toString()}||Utenti:${server.members.cache.filter(x=> !x.user.bot).size.toString()} \`\`\``, false)
-            .addField("Channels", `\`\`\`js\n Canali tot:${server.channels.cache.filter(x=> x.type == "GUILD_VOICE" || x.type == "GUILD_TEXT").size.toString()} || Canali vocali:${server.channels.cache.filter(x=> x.type == "GUILD_VOICE").size.toString()} ||Canali testuali: ${server.channels.cache.filter(x=> x.type == "GUILD_TEXT").size.toString()}  \`\`\``, false)
-            .addField("Server created", `\`\`\`js\n ${server.createdAt.toDateString()} \`\`\``, true)
-            .addField("Boost level", `\`\`\`js\n Level ${(server.premiumTier != "NONE" ? server.premiumTier : 0) + " (Boost: " + server.premiumSubscriptionCount + ")"} \`\`\``, true)
-            .setColor(configs.embed.color.green)
+            .setDescription(`Tutte le info su questo server`)
+            .addField(`Owner:`,`\`\`\`\n${server.members.cache.get(server.ownerId).nickname}\`\`\``)
+            .addField(`Livello di verifica:`,`\`\`\`\nAlto\`\`\``)
+            .addField(`Server id:`,`\`\`\`\n${server.id}\`\`\``)
+            .addField(`Members:`,` \`\`\`\n
+
+Membri tot: ${server.memberCount.toString()}
+
+Bot:${server.members.cache.filter(x => x.user.bot).size.toString()}
+
+Utenti:${server.members.cache.filter(x=> !x.user.bot).size.toString()}
+
+online: ${countingonline + countingidle}
+
+
+\`\`\``)
+
+.addField(`Channels:`,`\`\`\`\n
+
+Canali tot:${server.channels.cache.filter(x=> x.type == "GUILD_VOICE" || x.type == "GUILD_TEXT").size.toString()}
+
+Canali vocali:${server.channels.cache.filter(x=> x.type == "GUILD_VOICE").size.toString()} || Canali testuali: ${server.channels.cache.filter(x=> x.type == "GUILD_TEXT").size.toString()}
+
+\`\`\``)
+
+.addField( `Server created`, `\`\`\`\n${server.createdAt.toDateString()}\`\`\` `) 
+
+.addField(`Boost level Level`, `\`\`\`\n${(server.premiumTier != "NONE" ? server.premiumTier : 0) + " (Boost: " + server.premiumSubscriptionCount + ")"} \`\`\``)
+
+
         interaction.reply({ embeds: [embed] })
     }
 }
