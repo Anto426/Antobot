ba49d8b2a23c3a8a16d8006c5b93a68229950a23
Anto426 <amanocchio426@gmail.com>
2022-07-15 15:06:59 +0200

    v3,2

--- events/interaction/bottonandcommand/interactionCreatebotton.js
+++ events/interaction/bottonandcommand/interactionCreatebotton.js
@@ -77,14 +77,16 @@ module.exports = {
                         }
                         if (interaction.customId == "text+voice") {
                             type = "GUILD_TEXT"
-                            channelname = "「💭」" + interaction.member.user.username
-                            for (let i = 0; i < 2; i++) {
-                                let channel = await functions.createchannel(interaction, channelname, type, category, false)
+                            channelname = ["「💭」" + interaction.member.user.username, "「🔊」" + interaction.member.user.username]
+                            channelname.forEach(async x => {
+
+                                let channel = await functions.createchannel(interaction, x.toString(), type, category, false)
                                 functions.write(interaction, "room", channel, type2)
                                 type = "GUILD_VOICE"
-                                channelname = "「🔊」" + interaction.member.user.username
                                 type2 = true
-                            }
+                            })
+
+
                         }
 
 
