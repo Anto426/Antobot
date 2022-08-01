21eab0fa53510f826a4f93f947e31004d70a80cc
Anto426 <amanocchio426@gmail.com>
2022-08-01 22:57:52 +0200

    v4,0

--- function/update/updatestate.js
+++ function/update/updatestate.js
@@ -2,7 +2,7 @@ async function activity() {
 
     let frasi = [" moderare il server", "/help"]
     var x = Math.floor(Math.random() * frasi.length);
-    client.user.setActivity(x[frasi].toString());
+    client.user.setActivity(frasi[x].toString());
 
 }
 
