6f5bf935b043a5aaa123d257acaf96e27d356274
Sharkilmagnifico <giovannimastrangelo23@gmail.com>
2022-09-13 21:35:15 +0200

    negro dio

--- function/error/errormsg.js
+++ function/error/errormsg.js
@@ -1,6 +1,6 @@
 const configs = require("./../../index")
 function msggeneric(interaction) {
-    let frasi = ["Ops! Qualcosa è andato storto!!", "Riprova sarai più fortunato", "Ho riscrontrato alcuni errori durante l'esecuzione del comando"]
+    let frasi = ["Ops! Qualcosa è andato storto!!", "Riprova sarai più fortunato", "Ho riscrontrato alcuni errori durante l'esecuzione del comando","Mannaggia a quel preservativo bucato da cui sei nato porcodidido cambia lavoro fatti na vita e spoarati nell'ano porcodidido"]
     var x = Math.floor(Math.random() * frasi.length);
     const embed = new configs.Discord.EmbedBuilder()
         .setTitle("Error")
