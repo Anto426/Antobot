a957f1eb7ec0e0e3c795166a94df3f03170eac2b
Anto426 <amanocchio426@gmail.com>
2022-10-27 16:07:26 +0200

    update

--- function/feste/serverupdate.js
+++ function/feste/serverupdate.js
@@ -1,16 +1,15 @@
 const configs = require("./../../index")
 async function feste() {
     for (let i in configs.feste.mout) {
-        console.log(new Date().getMonth() + 3)
-        if (new Date().getMonth() + 3== i) {
-            console.log("yes")
+
+        if (new Date().getMonth() + 1 == i) {
+            console.log(i)
             configs.client.guilds.cache.forEach(f => {
                 f.setIcon(configs.feste.mout[i].photo)
                 f.channels.cache.forEach(x => {
                     for (let z in configs.feste.default.channel) {
-                        console.log(x.name, x.name.includes(configs.feste.default.channel[z]))
                         if (x.name.includes(configs.feste.default.channel[z]))
-                            x.setName(x.name.replace(configs.feste.default.channel[z], configs.feste.mout[new Date().getMonth() + 1].channel[z]))
+                            x.setName(x.name.replace(configs.feste.default.channel[z], configs.feste.mout[i].channel[z]))
                     }
                 });
             })
@@ -19,9 +18,10 @@ async function feste() {
             if (new Date().getMonth() == i) {
                 console.log("hi")
                 configs.client.guilds.cache.forEach(f => {
+                    f.setIcon(configs.feste.default.photo)
                     f.channels.cache.forEach(x => {
                         for (let z in configs.feste.mout[i].channel) {
-                            console.log(x.name,configs.feste.mout[i].channel[z],x.name.includes(configs.feste.mout[i].channel[z]))
+                            console.log(x.name, configs.feste.mout[i].channel[z], x.name.includes(configs.feste.mout[i].channel[z]))
                             if (x.name.includes(configs.feste.mout[i].channel[z]))
                                 x.setName(x.name.replace(configs.feste.mout[i].channel[z], configs.feste.default.channel[z]))
                         }
