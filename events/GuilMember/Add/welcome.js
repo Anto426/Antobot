1b9b9905e447ada5d4904a09029a074bf13be263
Anto426 <amanocchio426@gmail.com>
2022-12-31 18:40:10 +0100

    Create welcome.js

---
+++ events/GuilMember/Add/welcome.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js')
+const cembed = require("./../../../setting/embed.json")
+const cguild = require("./../../../setting/guild.json")
+module.exports = {
+    name: "guildMemberAdd",
+    async execute(member) {
+        console.log("hi")
+        const embed = new Discord.EmbedBuilder()
+            .setTitle(`Welcome`)
+            .setDescription(`${member} Welcome to ${member.guild.name}`)
+            .setThumbnail(member.user.displayAvatarURL({ dynamic: true, size: 4096 }))
+            .setColor(cembed.color["Gold Fusion"])
+
+        member.guild.channels.cache.get(cguild['Anto\'s  Server'].channel.serverinfo.welcome).send({ embeds: [embed] })
+        member.roles.add(member.guild.roles.cache.get(cguild['Anto\'s  Server'].role.user))
+
+    }
+}
