57abb49b16b4b83513a83d0896fc48e82c3188df
Anto426 <amanocchio426@gmail.com>
2023-01-01 13:25:18 +0100

    Update timef.js

--- functions/time/timef.js
+++ functions/time/timef.js
@@ -2,19 +2,15 @@ function times(millis) {
     var minutes = Math.floor(millis / 60000).toFixed(0);
     var hour = 0
     var day = 0
-    if (minutes >= 60) {
-        do {
-            hour = hour + 1
-            minutes = minutes - 60
-        } while (minutes >= 60)
+    while (minutes >= 60) {
+        hour = hour + 1
+        minutes = minutes - 60
     }
-    if (hour >= 24) {
-        do {
-            day = day + 1
-            hour = hour - 24
-        } while (hour >= 24)
+    while (hour >= 24) {
+        day = day + 1
+        hour = hour - 24
+    }
 
-    }
     return day + "d :" + hour + " h:" + minutes + " m"
 }
 
