0e019139e5cd99f84c02f78d41f05cb74393090e
Anto426 <amanocchio426@gmail.com>
2023-01-05 11:40:07 +0100

    Update ping.js

--- commands/general/ping.js
+++ commands/general/ping.js
@@ -1,20 +1,24 @@
+const { EmbedBuilder } = require("discord.js")
+const cembed = require("./../../setting/embed.json")
+const emoji = require("./../../setting/emoji.json")
 module.exports = {
     name: "ping",
-    permision: [],
-    onlyOwner: false,
-    defaultchannel: true,
-    allowedchannels:[],
+    permisions: [],
+    allowedchannels: [],
     data: {
         name: "ping",
         description: "Ping bot"
     },
     execute(interaction) {
-        var embed = new Discord.EmbedBuilder()
+        var embed = new EmbedBuilder()
             .addFields([
-                { name: 'Pong', value: `\`\`\`\n${client.ws.ping}ms\`\`\`` },
+                { name: `${emoji.pong} Pong`, value: `\`\`\`\n${client.ws.ping}ms\`\`\`` },
+                { name: `${emoji.ram} Ram`, value: `\`\`\`\n${(process.memoryUsage().heapUsed / 1048576).toFixed(0)}\`\`\`` },
+                { name: `${emoji.time} Time`, value: `\`\`\`\n${timeonc}\`\`\`` },
+
             ])
             .setThumbnail(client.user.displayAvatarURL({ dynamic: true }))
-            .setColor(configs.emded.color.Green)
+            .setColor(cembed.color.Green)
         interaction.reply({ embeds: [embed] })
     }
 }
