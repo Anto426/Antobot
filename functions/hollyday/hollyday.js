35c452bebf719f780ba2a081ac5c98223c5f76cd
Anto426 <amanocchio426@gmail.com>
2023-01-05 11:40:28 +0100

    Update hollyday.js

--- functions/hollyday/hollyday.js
+++ functions/hollyday/hollyday.js
@@ -5,11 +5,13 @@ const cembed = require("./../../setting/embed.json")
 const cguild = require("./../../setting/guild.json")
 
 
+
+
+
 async function nexhollyday() {
 
-    let i = 0, z = 0, timenow = 0, timenextholl = 0, festa
+    let i = 0, z = 0, timenextholl = 0, festa
     while (i == 0) {
-        timenow = new Date().getTime()
         timenextholl = new Date(hollyday.holidays[z].date).getTime()
         if (timenow < timenextholl) {
             festa = hollyday.holidays[z]
@@ -24,36 +26,30 @@ async function nexhollyday() {
 
 
 
-async function updatechannel() {
+async function updatechannel(guild) {
 
     let festa = await nexhollyday()
 
     let timenexthollyday = new Date(festa.date).getTime()
     setInterval(async () => {
-        let timenow = new Date().getTime()
         let timereminig = timenexthollyday - timenow
         let time = await `${times(timereminig)}`
-        client.guilds.cache.forEach(async guild => {
-            guild.channels.cache.get(cguild[guild.name].channel.hollyday.nexthollyday).setName(time.toString()).catch((err) => { console.log(err) })
-        })
+        guild.channels.cache.get(cguild["Anto's  Server"].channel.hollyday.nexthollyday).setName(time.toString()).catch((err) => { console.log(err) })
     }, 1000 * 60 * 5);
 
 }
 
 
 async function mainhollyday() {
+    let guild = await client.guilds.cache.get(cguild["Anto's  Server"].id)
     let festa = await nexhollyday()
-    client.guilds.cache.forEach(async guild => {
-        guild.channels.cache.get(cguild[guild.name].channel.hollyday.name).setName(`${festa.name.toString()}:`).catch((err) => { console.log(err) })
-    })
+    guild.channels.cache.get(cguild[guild.name].channel.hollyday.name).setName(`${festa.name.toString()}:`).catch((err) => { console.log(err) })
     let timenextholl = new Date(festa.date).getTime()
-    updatechannel()
+    updatechannel(guild)
     setInterval(() => {
-        timenow = new Date().getTime()
+
         let timereminig = timenextholl - timenow
         client.guilds.cache.forEach(async guild => {
-            let time = await `${festa.name}:${times(timereminig)}`
-            console.log(time)
             if (timereminig <= 0) {
                 console.log(festa.phrase)
                 const embed = new Discord.EmbedBuilder()
