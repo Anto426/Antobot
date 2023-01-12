eb51f1b3449e42e12548ed4af6a6d1dfb73bddb6
Anto426 <amanocchio426@gmail.com>
2023-01-12 18:18:50 +0100

    init client distube

--- functions/client/intclient.js
+++ functions/client/intclient.js
@@ -1,4 +1,8 @@
 const { Client, Partials } = require('discord.js');
+const { DisTube } = require("distube")
+const { SpotifyPlugin } = require("@distube/spotify")
+const { SoundCloudPlugin } = require("@distube/soundcloud")
+const { YtDlpPlugin } = require('@distube/yt-dlp');
 
 
 async function intclient() {
@@ -6,6 +10,19 @@ async function intclient() {
         intents: 3276799,
         partials: [Partials.User, Partials.Reaction, Partials.Message, Partials.Channel]
     });
+    global.distube = new DisTube(client, {
+        leaveOnStop: true,
+        emitNewSongOnly: true,
+        emitAddSongWhenCreatingQueue: false,
+        emitAddListWhenCreatingQueue: false,
+        plugins: [
+            new SpotifyPlugin({
+                emitEventsAfterFetching: true
+            }),
+            new SoundCloudPlugin(),
+            new YtDlpPlugin()
+        ]
+    })
 }
 
 module.exports = {
