8cc7a0300eaba758fa92fd40356ba43d1cb86468
Anto426 <amanocchio426@gmail.com>
2023-01-12 20:11:50 +0100

    Create eval.js

---
+++ commands/bot/eval.js
@@ -0,0 +1,46 @@
+const { EmbedBuilder } = require('discord.js')
+const { inspect } = require(`util`)
+const cembed = require("./../../setting/embed.json")
+const cguild = require("./../../setting/guild.json")
+module.exports = {
+    name: "eval",
+    permisions: [],
+    allowedchannels: [cguild["Anto's  Server"].channel.general.command, cguild["Anto's  Server"].channel.temp.command],
+    data: {
+        name: "eval",
+        description: "Comando per eseguire del codice js",
+        options: [{
+            name: "comand",
+            description: "comando",
+            type: 3,
+            required: true
+        }]
+    },
+    async execute(interaction) {
+
+        let args = interaction.options.getString("comand")
+
+        try {
+            evaled = await eval(args);
+            const embed = new EmbedBuilder()
+                .setTitle("Comando eseguito con successo")
+                .setColor(cembed.color['Green Blue'])
+                .setDescription(`Non ci sono stati errori durante l 'esecuzione del comando
+                output :\`\`\`js\n ${inspect((evaled))}  \`\`\``)
+            interaction.reply({ embeds: [embed] })
+        } catch (error) {
+            console.error(error);
+            const embed = new EmbedBuilder()
+                .setTitle("Error")
+                .setColor(cembed.color.Red)
+                .setDescription("Ho riscrontrato alcuni errori!!")
+                .addFields([
+                    { name: 'Input:', value: `\`\`\`js\n ${args}  \`\`\`` },
+                    { name: 'Error:', value: `\`\`\`js\n ${inspect((error.toString()))}  \`\`\`` },
+                ])
+                .setThumbnail(cembed.immage.err)
+            interaction.reply({ embeds: [embed] })
+        }
+
+    }
+}
