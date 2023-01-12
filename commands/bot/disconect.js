37ce43a465ed451f848b8fbd5fa559994cd164ed
Anto426 <amanocchio426@gmail.com>
2023-01-12 15:43:13 +0100

    Create disconect.js

---
+++ commands/bot/disconect.js
@@ -0,0 +1,27 @@
+const { EmbedBuilder } = require("discord.js")
+const cembed = require("./../../setting/embed.json")
+const cguild = require("./../../setting/guild.json")
+const fs = require("fs")
+const patch = "./.env"
+module.exports = {
+    name: "disconect",
+    permisions: [],
+    allowedchannels: [cguild["Anto's  Server"].channel.general.command, cguild["Anto's  Server"].channel.temp.command],
+    data: {
+        name: "disconect",
+        description: "disconette il client dal bot"
+    },
+    async execute(interaction) {
+        console.log("disconect.......")
+        var embed = new EmbedBuilder()
+            .setTitle("disconecting")
+            .setThumbnail(cembed.immage.load)
+            .setColor(cembed.color.Yellow)
+        await interaction.reply({ embeds: [embed] })
+        await fs.unlinkSync(patch);
+        await client.destroy()
+        process.exit()
+
+
+    }
+}
