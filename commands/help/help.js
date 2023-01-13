7df734845b1ef0a11fc73a791833d93abecccaa4
Anto426 <amanocchio426@gmail.com>
2023-01-13 21:59:20 +0100

    Update help.js

--- commands/help/help.js
+++ commands/help/help.js
@@ -6,6 +6,7 @@ module.exports = {
     name: "help",
     permisions: [],
     allowedchannels: [cguild["Anto's  Server"].channel.general.command, cguild["Anto's  Server"].channel.temp.command],
+    position: false,
     data: {
         name: "help",
         description: "commando help"
