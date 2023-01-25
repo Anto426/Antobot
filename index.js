6caacd654b905046ada57b0b046f83f5881e706a
Anto426 <amanocchio426@gmail.com>
2023-01-25 19:32:43 +0100

    clean

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // New version of Anto bot v 5.0 
 // const
-const fs = require("fs");
 const { intclient } = require('./functions/client/intclient');
 const { eventload } = require("./functions/client/loadc-e");
 const { tokenload } = require('./functions/client/tokenload');
