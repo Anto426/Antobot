fdcb6df7bcc902e45128ba8d546666f29f4e9d90
Anto426 <amanocchio426@gmail.com>
2023-01-27 20:52:23 +0100

    Create msg.js

---
+++ functions/msg/msg.js
@@ -0,0 +1,23 @@
+const cembed = require("./../../setting/embed.json")
+const { EmbedBuilder } = require("discord.js")
+
+
+async function sendto(user, content, channel) {
+
+    try {
+        user.send(content).catch((err) => {
+            const embed = new EmbedBuilder()
+            .setTitle("Error")
+            .setColor(cembed.color.Red)
+            .setDescription(user.toString() + " ha i dm chiusi non posso contattarlo in privato")
+            channel.send({ embeds: [embed] })
+        })
+    } catch (err) {
+
+    }
+
+
+
+}
+module.exports = { sendto }
+
