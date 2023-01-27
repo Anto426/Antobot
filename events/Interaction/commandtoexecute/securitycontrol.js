6b2a7c2f77a1c16caa7149da5b92629e1edfd28b
Anto426 <amanocchio426@gmail.com>
2023-01-27 19:15:26 +0100

    fix

--- events/Interaction/commandtoexecute/securitycontrol.js
+++ events/Interaction/commandtoexecute/securitycontrol.js
@@ -5,7 +5,7 @@ module.exports = {
     name: "interactionCreate-commands",
     async execute(interaction) {
         if (!interaction.isChatInputCommand()) return;
-        let owner = false, sowner = false, staff = false, perm = false, channel = false, position = false, test = false, execute = false
+        let owner = false, sowner = false, staff = false, perm = false, channel = false, position = false, test = true, execute = false
         const command = client.commands.get(interaction.commandName)
         const ow = require("./../../../setting/onwer.json")
 
@@ -21,8 +21,8 @@ module.exports = {
             sowner = true
         }
 
-        if ((!command.test) && owner) {
-            test = true
+        if (command.test && !owner) {
+            test = false
         }
         if (command.permisions.length == 0) {
             perm = true
