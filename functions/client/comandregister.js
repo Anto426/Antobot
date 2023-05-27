4ae6947bec45a7fa167364c82b063011110d7bd3
Admin <amanocchio426@gmail.com>
2023-05-27 09:11:49 +0200

    fix

--- functions/client/comandregister.js
+++ functions/client/comandregister.js
@@ -1,8 +1,9 @@
 const cguild = require("./../../settings/guild.json")
 function comandregisterguild() {
     try {
-        console.log("sto scrivendo  " + client.commands.size + " comandi " + " in " + client.guilds.cache.find(x => x.id == cguild["Anto's  Server"].id).name)
+        console.log("sto aggiornando  " + client.commands.size + " comandi " + " in " + client.guilds.cache.find(x => x.id == cguild["Anto's  Server"].id).name)
         client.commands.forEach(command => {
+            client.guilds.cache.find(x => x.id == cguild["Anto's  Server"].id).commands.delete().catch((err) => { console.log(err.toString()) })
             client.guilds.cache.find(x => x.id == cguild["Anto's  Server"].id).commands.create(command.data).catch((err) => { console.log(err.toString()) })
 
         })
