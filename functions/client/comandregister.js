0605eede8c8a8229c1187062550459d3813eb2fa
Admin <amanocchio426@gmail.com>
2023-05-27 09:12:52 +0200

    fix

--- functions/client/comandregister.js
+++ functions/client/comandregister.js
@@ -3,7 +3,7 @@ function comandregisterguild() {
     try {
         console.log("sto aggiornando  " + client.commands.size + " comandi " + " in " + client.guilds.cache.find(x => x.id == cguild["Anto's  Server"].id).name)
         client.commands.forEach(command => {
-            client.guilds.cache.find(x => x.id == cguild["Anto's  Server"].id).commands.delete().catch((err) => { console.log(err.toString()) })
+            client.guilds.cache.find(x => x.id == cguild["Anto's  Server"].id).commands.delete(command.data).catch((err) => { console.log(err.toString()) })
             client.guilds.cache.find(x => x.id == cguild["Anto's  Server"].id).commands.create(command.data).catch((err) => { console.log(err.toString()) })
 
         })
