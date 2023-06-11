54ba067a7a34b2f62d2152a8403e937e38dcfdbf
Anto426 <amanocchio426@gmail.com>
2023-06-11 10:13:04 +0200

    fix

--- functions/count/hollydaycount.js
+++ functions/count/hollydaycount.js
@@ -23,7 +23,7 @@ async function updatecount(festa, channelcount) {
     try {
         do {
             setTimeout(() => {
-                let timereminig = new Date(new Date().getFullYear(), festa.date.mouth, festa.date.day) - new Date().getTime()
+                var timereminig = new Date(new Date().getFullYear(), festa.date.mouth, festa.date.day) - new Date().getTime()
                 let time = `${times(timereminig)}`
                 console.log(`Update : ${time}`)
                 channelcount.setName(time.toString()).catch((err) => { console.log(err.toString()) })
@@ -39,7 +39,7 @@ async function sendcongratulations(festa) {
 
     do {
         setTimeout(() => {
-            let timereminig = new Date(new Date().getFullYear(), festa.date.mouth, festa.date.day) - new Date().getTime()
+            var timereminig = new Date(new Date().getFullYear(), festa.date.mouth, festa.date.day) - new Date().getTime()
             if (timereminig <= 0) {
                 console.log(festa.title)
                 congratulatioembed(festa)
