003aa09c422305f76e172e5cdb5f0dc82f6c3423
Anto426 <amanocchio426@gmail.com>
2023-06-14 21:59:46 +0200

    fix

--- events/GuilMember/update.js
+++ events/GuilMember/update.js
@@ -5,6 +5,7 @@ module.exports = {
     name: 'guildMemberUpdate',
     async execute(oldMember, newMember) {
         try {
+            console.log(oldMember.premiumSince)
             if (oldMember.premiumSince == null && newMember.premiumSince != null)
                 boostembed(newMember)
         } catch (err) { console.log(err) }
