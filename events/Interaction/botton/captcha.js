81d507057433c003cc19b336175f7147e5b33952
Anto426 <amanocchio426@gmail.com>
2023-06-19 12:28:02 +0200

    fix

--- events/Interaction/botton/captcha.js
+++ events/Interaction/botton/captcha.js
@@ -1,10 +1,11 @@
 const { Cautor } = require('../../../functions/interaction/checkautorinteraction');
-const { ActionRowBuilder, ChannelType, PermissionFlagsBits } = require('discord.js');
+const { ChannelType, PermissionFlagsBits } = require('discord.js');
 const { genericerr } = require('../../../embeds/err/generic');
 const cguild = require("./../../../settings/guild.json")
 const { createrowcaptcha, createrowstartchanneldelete } = require("../../../functions/row/createrow");
 const { captchaembed, captchaembedsucc, captchaembednotv, captchadelbackup } = require("../../../embeds/moderation/captcha");
 const { welcomeembed, logaddmember, logaddmembernotv } = require("../../../embeds/GuilMember/addembed");
+const { disablebotton } = require('../../../functions/interaction/disablebotton');
 
 module.exports = {
     name: "interactionCreate",
@@ -15,9 +16,8 @@ module.exports = {
 
             if (interaction.customId.split("-").includes("capchastart")) {
                 if (Cautor(interaction)) {
-
+                    disablebotton(interaction)
                     let row = createrowcaptcha(interaction.guild.members.cache.find(x => x.id == interaction.customId.split("-")[1]), interaction.customId.split("-")[2])
-
                     captchaembed(interaction.guild.members.cache.find(x => x.id == interaction.customId.split("-")[1]), row, interaction)
 
                 }
@@ -31,23 +31,11 @@ module.exports = {
 
                     let category = interaction.guild.channels.cache.find(x => x.name == "╚»★«╝ verifica ╚»★«╝")
                     let category1 = interaction.guild.channels.cache.find(x => x.name == "╚»★«╝ Backup ╚»★«╝")
-
-                    const originalRow = interaction.message.components.find(component => component.type === 1);
-
-                    // Creare una copia del componente 'row' originale
-                    let updatedRow = new ActionRowBuilder().addComponents(...originalRow.components);
-
-                    updatedRow.components.forEach(button => {
-                        button.data.disable = true
-                        console.log(button)
-                    });
-                    console.log(updatedRow)
-                    await interaction.message.edit({ components: [updatedRow] })
-
+                    disablebotton(interaction)
 
                     if (interaction.customId.split("-")[3] == "t") {
                         let [bots, humans] = (await interaction.guild.members.fetch()).partition(member => member.user.bot);
-                        captchaembedsucc(interaction.member, interaction)
+                        await captchaembedsucc(interaction.member, interaction)
                         logaddmember(interaction.member, humans.size)
                         welcomeembed(interaction.member, humans.size)
                         interaction.member.roles.add(interaction.guild.roles.cache.get(cguild["Anto's  Server"].role.user))
@@ -79,7 +67,6 @@ module.exports = {
                         captchadelbackup(interaction.channel, row)
 
                     }, 60 * 1000)
-
 
                 }
 
