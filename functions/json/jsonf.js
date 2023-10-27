6dc76d8bf6d99f1fb110fe98d9bd2fac774b9204
Anto426 <amanocchio426@gmail.com>
2023-10-27 09:02:59 +0200

    fix

--- functions/json/jsonf.js
+++ functions/json/jsonf.js
@@ -3,15 +3,18 @@ const { checkvs } = require("../check/check");
 
 
 function jsonwu(filePath, parr, newData) {
+    let retur = true
     fs.readFile(filePath, 'utf8', (err, data) => {
         if (err) {
             console.error('Errore nella lettura del file:', err);
+            retur = false
         } else {
             try {
+
                 const obj = JSON.parse(data);
                 if (checkvs(obj[parr], newData)) {
                     console.log("Valore gia presente");
-                    return false;
+                    retur = false;
                 }
                 obj.list.push(newData);
                 const json = JSON.stringify(obj, null, 2);
@@ -20,12 +23,13 @@ function jsonwu(filePath, parr, newData) {
                         console.error('Errore nella scrittura del file:', err);
                     } else {
                         console.log('Data aggiunto', filePath);
-                        return true
                     }
                 });
             } catch (error) {
                 console.error('Errore:', error);
+                retur = false
             }
+            return retur;
         }
     });
 }
