5bb789064473424935cf54df8ebf559bc20328c0
Anto426 <amanocchio426@gmail.com>
2023-12-17 16:33:50 +0100

    update

--- function/security/security.js
+++ function/security/security.js
@@ -3,11 +3,14 @@ const { check } = require("../check/check")
 class securyty extends check {
 
     constructor() {
-        this.owner
-        this.Sowner
-        this.staff
-        this.higtofp
+        this.owner = false
+        this.Sowner = false
+        this.staff = false
+        this.higtofp = false
+        this.position = false
+        this.channel = false
     }
+
 
 
     chekowner(arr, id) {
@@ -31,7 +34,35 @@ class securyty extends check {
             })
 
     }
-    
+    checkpchannel(idchannel, arr) {
+        super.checkpchannel(idchannel, arr)
+            .then(() => {
+                this.channel = true;
+            })
+            .catch(() => {
+
+            })
+    }
+
+    checkpermision(iduser, idguild) {
+        super.checkpermision(iduser, idguild)
+            .then(() => {
+                this.staff = true;
+            })
+            .catch(() => {
+
+            })
+    }
+
+    checkposition(iduser, otheruserid, guildid) {
+        super.checkposition(iduser, otheruserid, guildid)
+            .then(() => {
+                this.position = true;
+            })
+            .catch(() => {
+
+            })
+    }
 
 }
 
