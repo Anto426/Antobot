765e8564c614d2fa79dd2b80abef2927e454e068
Anto426 <amanocchio426@gmail.com>
2023-12-17 16:32:38 +0100

    update log

--- function/log/bootlog.js
+++ function/log/bootlog.js
@@ -1,40 +1,94 @@
 const packageI = require("./../../package.json");
-const { consolelog } = require("./consolelog");
+const Table = require('cli-table3');
 
 class Info {
     constructor() {
-        this.nome_pacchetto = packageI.name.charAt(0).toUpperCase() + packageI.name.slice(1);
-        this.versione = packageI.version;
         this.autore = packageI.author.charAt(0).toUpperCase() + packageI.author.slice(1);
         this.versione_discordjs = packageI.dependencies["discord.js"];
         this.versione_nodejs = process.version.charAt(0).toUpperCase() + process.version.slice(1);
         this.link_repo = packageI.repo;
         this.nome_client = client.user.username.charAt(0).toUpperCase() + client.user.username.slice(1);
-        this.token = client.token
+        this.token = client.token;
         this.nguild = client.guilds.cache.size;
-        this.ram = `${process.memoryUsage().heapUsed / 1024 / 1024} mb`;
+        this.ram = `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} mb`;
         this.link_invito = `https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=applications.commands%20bot`;
-        this.ncomandi = client.commands ? client.commands.size : 0;
-        this.neventi = client.events ? client.events.size : 0;
+
+
     }
 
     log() {
         try {
-            for (let attributo of Object.getOwnPropertyNames(this)) {
-                consolelog(`${attributo.charAt(0).toUpperCase() + attributo.slice(1)}: ${this[attributo]}\n`);
+            const settable = {
+                'top': '', 'top-mid': '', 'top-left': '╔', 'top-right': '╗',
+                'bottom': '', 'bottom-mid': '', 'bottom-left': '╚', 'bottom-right': '╝',
+                'left': '║', 'left-mid': '╟', 'mid': '─', 'mid-mid': '┼',
+                'right': '║', 'right-mid': '╢', 'middle': '│'
             }
-        } catch {
-            consolelog("Errore nel caricamento del log iniziale")
-        }
+            const GeneralTable = new Table({
+                head: [`${packageI.name.charAt(0).toUpperCase() + packageI.name.slice(1)} v `, packageI.version],
+                colWidths: [25, 117],
+                style: {
+                    head: ['yellow'],
+                    border: ['white'],
+                    compact: false,
+                    wordWrap: true,
+                },
+                chars: settable,
 
+            });
+
+
+            const EventsTable = new Table({
+                head: ["Event Load:" + [client.events.size]],
+                colWidths: [61],
+                style: {
+                    head: [client.events.size > 0 ? 'green' : 'red'],
+                    border: ['white'],
+                    compact: false
+                },
+                chars: settable,
+            });
+
+
+
+
+            const ComandsTable = new Table({
+                head: ["Comand Load:" + [client.commands.size]],
+                colWidths: [20, 40],
+                style: {
+                    head: [client.commands.size > 0 ? 'green' : 'red'],
+                    border: ['white'],
+                    compact: false
+                },
+                chars: settable
+            });
+
+            client.events.forEach(element => {
+                EventsTable.push(["Name:" + [element.name]])
+            });
+
+            client.commands.forEach(element => {
+                ComandsTable.push(["Name:" + element.name, "Description:" + element.data.description])
+            });
+
+
+
+
+            for (let attributo of Object.getOwnPropertyNames(this)) {
+                if (this[attributo]) {
+                    const value = this[attributo];
+                    GeneralTable.push({ [attributo.charAt(0).toUpperCase() + attributo.slice(1)]: value });
+                }
+            }
+
+            console.log(GeneralTable.toString())
+
+            console.log(ComandsTable.toString())
+
+            console.log(EventsTable.toString())
+
+        } catch (err) { console.log(err) }
     }
+
 }
-
-
-
-
-
-
-
-
 module.exports = { Info }
