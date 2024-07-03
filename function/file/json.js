83b2647acf25b785677cf4e45a4c5bc5971e0c7e
Anto426 <amanocchio426@gmail.com>
2024-07-03 17:40:40 +0200

    fixed non-permanently creating multiple oldemembers files

--- function/file/json.js
+++ function/file/json.js
@@ -41,7 +41,7 @@ class json {
 
     createJSONFile(filePath, dataObject) {
         return new Promise((resolve, reject) => {
-
+            filePath = filePath.filter(".json", "");
             const jsonData = JSON.stringify(dataObject, null, 2);
 
             let dirpatch = filePath.substring(0, filePath.lastIndexOf('/'));
