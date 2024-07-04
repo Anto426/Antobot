13b4b37e40f83fcca1771cebe82a0bb502417d9b
Anto426 <amanocchio426@gmail.com>
2024-07-04 12:22:19 +0200

    fix

--- base/events/GuildMember/addM.js
+++ base/events/GuildMember/addM.js
@@ -13,46 +13,49 @@ module.exports = {
 
         if (!member.bot) {
             let embedmsg = new eventbembed(member.guild)
-            embedmsg.init().catch(() => { consolelog("Non sono riuscito a iniziallizzare embed di base", "red") })
-            let json = new Cjson();
-            await json.readJson(process.env.dirdatabase + setting.database.root + "/" + setting.database.listoldmebers).then(async (jsonf) => {
-                if (jsonf[member.guild.id][member.id]) {
-                    let listrole = [];
-                    jsonf[member.guild.id][member.id].roles.forEach(element => {
-                        let role = member.guild.roles.cache.find(x => x.id === element);
-                        if (role)
-                            if (!member.roles.cache.has(role.id)) {
-                                try {
-                                    listrole.push(role.name);
-                                    member.roles.add(role);
-                                } catch { }
-                            }
+            embedmsg.init().then(async () => {
 
-                    });
+                let json = new Cjson();
+                await json.readJson(process.env.dirdatabase + setting.database.root + "/" + setting.database.listoldmebers).then(async (jsonf) => {
+                    if (jsonf[member.guild.id][member.id]) {
+                        let listrole = [];
+                        jsonf[member.guild.id][member.id].roles.forEach(element => {
+                            let role = member.guild.roles.cache.find(x => x.id === element);
+                            if (role)
+                                if (!member.roles.cache.has(role.id)) {
+                                    try {
+                                        listrole.push(role.name);
+                                        member.roles.add(role);
+                                    } catch { }
+                                }
 
-                    let send = await embedmsg.welcomeback(member, listrole).catch((err) => { consolelog(err); consolelog("Non sono riuscito ad inviare il messaggio", "red") })
-                    member.send({ embeds: [send] })
+                        });
+
+                        let send = await embedmsg.welcomeback(member, listrole).catch(() => { consolelog("Non sono riuscito ad inviare il messaggio", "red") })
+                        member.send({ embeds: [send] })
 
 
-                } else {
+                    } else {
 
-                    let [bots, humans] = (await member.guild.members.fetch()).partition(member => member.user.bot);
-                    json.jsonddypendencebufferolyf(setting.configjson.online.url + "/" + setting.configjson.online.name[2], process.env.GITTOKEN).then((jsonf0) => {
-                        embedmsg.init().then(async () => {
-                            let send = await embedmsg.welcome(member, humans.size).catch(() => { })
-                            member.guild.channels.cache.find(x => x.id == jsonf0["Anto's  Server"].channel.info.welcome).send({ embeds: [send[0]], files: [send[1]] })
+                        let [bots, humans] = (await member.guild.members.fetch()).partition(member => member.user.bot);
+                        json.jsonddypendencebufferolyf(setting.configjson.online.url + "/" + setting.configjson.online.name[2], process.env.GITTOKEN).then((jsonf0) => {
+                            embedmsg.init().then(async () => {
+                                let send = await embedmsg.welcome(member, humans.size).catch(() => { })
+                                member.guild.channels.cache.find(x => x.id == jsonf0["Anto's  Server"].channel.info.welcome).send({ embeds: [send[0]], files: [send[1]] })
 
-                        }).catch(() => { })
-                        try {
-                            member.roles.add(member.guild.roles.cache.find(x => x.id === jsonf0["Anto's  Server"].role.user))
-                        } catch { }
+                            }).catch(() => { })
+                            try {
+                                member.roles.add(member.guild.roles.cache.find(x => x.id === jsonf0["Anto's  Server"].role.user))
+                            } catch { }
 
-                    })
+                        })
 
-                }
-            }).catch(async (err) => {
+                    }
+                }).catch(async (err) => {
 
-            })
+                })
+            }).catch(() => { consolelog("Non sono riuscito a iniziallizzare embed di base", "red") })
+
         } else {
 
             try {
