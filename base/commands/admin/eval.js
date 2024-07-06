1dbe9d2e28ce49b19a5a4a1f915eb698ebfb3a62
Anto426 <amanocchio426@gmail.com>
2024-07-06 13:51:53 +0200

    fix

--- base/commands/admin/eval.js
+++ base/commands/admin/eval.js
@@ -40,6 +40,7 @@ module.exports = {
                     );
 
             } catch (error) {
+                consolelog(error)
                 embedmsg
                     .setDescription("Comando non eseguito")
             }
