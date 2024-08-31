7f68f794ae425d9eb9f669365b91b5f80fa66eb1
Anto426 <amanocchio426@gmail.com>
2024-09-01 00:54:38 +0200

    Update Distube initialization with YtDlpPlugin and cookies support

--- function/client/initclient.js
+++ function/client/initclient.js
@@ -70,47 +70,59 @@ class ClientInit {
         return new Promise(async (resolve, reject) => {
 
             try {
-                global.distube = new DisTube(client, {
-                    leaveOnStop: true,
-                    emitNewSongOnly: true,
-                    emitAddSongWhenCreatingQueue: false,
-                    emitAddListWhenCreatingQueue: false,
-                    plugins: [
-                        new SpotifyPlugin({
-                            emitEventsAfterFetching: true
-                        }),
-                        new SoundCloudPlugin(),
-                        new YtDlpPlugin()
-                    ],
-                    customFilters: {
-                        'bassboost': 'bass=g=10',
-                        '8D': 'apulsator=hz=0.08',
-                        'vaporwave': 'aresample=48000,asetrate=48000*0.8',
-                        'nightcore': 'aresample=48000,asetrate=48000*1.25',
-                        'phaser': 'aphaser=in_gain=0.4',
-                        'tremolo': 'tremolo',
-                        'vibrato': 'vibrato=f=6.5',
-                        'reverse': 'areverse',
-                        'treble': 'treble=g=5',
-                        'normalizer': 'dynaudnorm=g=101',
-                        'surrounding': 'surround',
-                        'pulsator': 'apulsator=hz=1',
-                        'subboost': 'asubboost',
-                        'karaoke': 'stereotools=mlev=0.03',
-                        'flanger': 'flanger',
-                        'gate': 'agate',
-                        'haas': 'haas',
-                        'mcompand': 'mcompand',
-                        'earwax': 'earwax',
-                    },
-                    ytdlOptions: {
-                        quality: 'highestaudio',
-                        highWaterMark: 1 << 25,
-                        filter: 'audioonly',
-                    },
-                })
-                new BotConsole().log("Client di Distube inzializato con successo", "green");
-                resolve(0)
+                this.json.jsonDependencyBuffer(setting.configjson.online.url + "/" + setting.configjson.online.name[7], process.env.GITTOKEN)
+                    .then((cookies) => {
+                        global.distube = new DisTube(client, {
+                            leaveOnStop: true,
+                            emitNewSongOnly: true,
+                            emitAddSongWhenCreatingQueue: false,
+                            emitAddListWhenCreatingQueue: false,
+                            plugins: [
+                                new SpotifyPlugin({
+                                    emitEventsAfterFetching: true
+                                }),
+                                new SoundCloudPlugin(),
+                                new YtDlpPlugin(
+                                    {
+                                        cookies: [
+                                            {
+                                                cookies: cookies.youtube,
+                                            }
+                                        ]
+                                    }
+                                )
+                            ],
+                            customFilters: {
+                                'bassboost': 'bass=g=10',
+                                '8D': 'apulsator=hz=0.08',
+                                'vaporwave': 'aresample=48000,asetrate=48000*0.8',
+                                'nightcore': 'aresample=48000,asetrate=48000*1.25',
+                                'phaser': 'aphaser=in_gain=0.4',
+                                'tremolo': 'tremolo',
+                                'vibrato': 'vibrato=f=6.5',
+                                'reverse': 'areverse',
+                                'treble': 'treble=g=5',
+                                'normalizer': 'dynaudnorm=g=101',
+                                'surrounding': 'surround',
+                                'pulsator': 'apulsator=hz=1',
+                                'subboost': 'asubboost',
+                                'karaoke': 'stereotools=mlev=0.03',
+                                'flanger': 'flanger',
+                                'gate': 'agate',
+                                'haas': 'haas',
+                                'mcompand': 'mcompand',
+                                'earwax': 'earwax',
+                            },
+                            ytdlOptions: {
+                                quality: 'highestaudio',
+                                highWaterMark: 1 << 25,
+                                filter: 'audioonly',
+                            },
+                        })
+                        new BotConsole().log("Client di Distube inzializato con successo", "green");
+                        resolve(0)
+
+                    }).catch(() => { new BotConsole().log("Errore variabile json non caricata", "red") });
             } catch (err) {
                 new BotConsole().log("Errore nel inizializare il client di Distube", "red");
                 reject(err);
