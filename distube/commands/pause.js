00ba6ae964793f462fcce919cb72df10b17d7b85
Anto426 <amanocchio426@gmail.com>
2024-09-01 17:22:14 +0200

    feat: Add pause and resume commands for Distube

---
+++ distube/commands/pause.js
@@ -0,0 +1,52 @@
+const { CommandEmbed } = require("../../embed/distube/command");
+const { ErrEmbed } = require("../../embed/err/errembed");
+
+module.exports = {
+    name: "pause",
+    permisions: [],
+    allowedchannels: true,
+    position: false,
+    test: false,
+    see: true,
+    disTube: {
+        checkchannel: true,
+        checklisttrack: false
+    },
+    data: {
+        name: "pause",
+        description: "metti in pausa la canzone",
+    },
+    async execute(interaction) {
+
+
+        let queue = interaction.client.distube.getQueue(interaction.guildId)
+        let embedmsg = new CommandEmbed(interaction.guild, interaction.member)
+
+
+        embedmsg.init()
+            .then(async () => {
+                queue.pause().then(() => {
+                    interaction.reply({ embeds: [embedmsg.repeat(mode)] })
+                }).catch((err) => {
+                    let embedmsg = new ErrEmbed(interaction.guild, interaction.member)
+                    embedmsg.init().then(() => {
+                        interaction.reply({ embeds: [embedmsg.notpauseError()], ephemeral: true })
+                    }).catch((err) => {
+                        console.error(err);
+                    })
+
+                })
+            }).catch((err) => {
+                console.error(err);
+                let embedmsg = new ErrEmbed(interaction.guild, interaction.member);
+                embedmsg.init().then(() => {
+                    interaction.reply({ embeds: [embedmsg.genericError()], ephemeral: true });
+                }).catch((err) => {
+                    console.error(err);
+                });
+            });
+
+
+    }
+
+}
---
+++ distube/commands/reasume.js
@@ -0,0 +1,51 @@
+const { CommandEmbed } = require("../../embed/distube/command");
+const { ErrEmbed } = require("../../embed/err/errembed");
+
+module.exports = {
+    name: "resume",
+    permisions: [],
+    allowedchannels: true,
+    position: false,
+    test: false,
+    see: true,
+    disTube: {
+        checkchannel: true,
+        checklisttrack: false
+    },
+    data: {
+        name: "resume",
+        description: "riprende la canzone",
+    },
+    async execute(interaction) {
+
+
+        let queue = interaction.client.distube.getQueue(interaction.guildId)
+        let embedmsg = new CommandEmbed(interaction.guild, interaction.member)
+
+        embedmsg.init()
+            .then(async () => {
+                queue.pause().then(() => {
+                    interaction.reply({ embeds: [embedmsg.repeat(mode)] })
+                }).catch(() => {
+                    let embedmsg = new ErrEmbed(interaction.guild, interaction.member)
+                    embedmsg.init().then(() => {
+                        interaction.reply({ embeds: [embedmsg.notresumeError()], ephemeral: true })
+                    }).catch((err) => {
+                        console.error(err);
+                    })
+
+                })
+            }).catch((err) => {
+                console.error(err);
+                let embedmsg = new ErrEmbed(interaction.guild, interaction.member);
+                embedmsg.init().then(() => {
+                    interaction.reply({ embeds: [embedmsg.genericError()], ephemeral: true });
+                }).catch((err) => {
+                    console.error(err);
+                });
+            });
+
+
+    }
+
+}
