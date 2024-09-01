9b25a28ab43d76638a832af50525b8e2f0042a1a
Anto426 <amanocchio426@gmail.com>
2024-09-01 18:12:53 +0200

    fix

--- distube/events/error.js
+++ distube/events/error.js
@@ -3,22 +3,27 @@ const { ErrEmbed } = require("../../embed/err/errembed");
 
 module.exports = {
     name: "Error of distube",
-    typeEvent: "ffmpegDebug",
+    typeEvent: "error",
     async execute(error, queue, song) {
-        let embedmsg = new EventEmbed(queue.textChannel.guild);
-        console.log(error);
-        embedmsg.init().then(() => {
-            queue.textChannel.send({ embeds: [embedmsg.error(song)], ephemeral: true });
-        }).catch((err) => {
-            console.error(err);
-            let embedmsg = new ErrEmbed(queue.textChannel.guild);
+        try {
+            console.log(error);
+            let embedmsg = new EventEmbed(queue.textChannel.guild);
             embedmsg.init().then(() => {
-                interaction.reply({ embeds: [embedmsg.genericError()], ephemeral: true });
+                queue.textChannel.send({ embeds: [embedmsg.error(song)], ephemeral: true });
             }).catch((err) => {
                 console.error(err);
-            });
+                let embedmsg = new ErrEmbed(queue.textChannel.guild);
+                embedmsg.init().then(() => {
+                    interaction.reply({ embeds: [embedmsg.genericError()], ephemeral: true });
+                }).catch((err) => {
+                    console.error(err);
+                });
 
-        });
+            });
+        }catch(err){
+            console.error(err);
+        }
+
     }
 }
 
