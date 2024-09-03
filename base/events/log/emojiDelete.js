598472046e32dee240a5a6d8b1bfbe0e8b7da714
Anto426 <amanocchio426@gmail.com>
2024-09-03 14:03:45 +0200

    refactor: Update emojiDelete event logging functionality

--- base/events/log/emojiDelete.js
+++ base/events/log/emojiDelete.js
@@ -8,7 +8,7 @@ module.exports = {
         let logchannel = new log();
         let console = new BotConsole();
         logchannel.init().then(() => {
-            logchannel.emojiCreate(emoji, tag);
+            logchannel.emojiDelete(emoji, tag);
         }).catch(() => { console.log("Errore nell'inizializzare il modulo log", "red") });
     }
 }
