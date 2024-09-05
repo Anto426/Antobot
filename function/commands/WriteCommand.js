26cec053fbaf0475d5856b4e0034af61d3abcb69
Anto426 <amanocchio426@gmail.com>
2024-09-05 03:42:55 +0200

    update  WriteCommand

--- function/commands/WriteCommand.js
+++ function/commands/WriteCommand.js
@@ -4,152 +4,194 @@ const { BotConsole } = require("../log/botConsole");
 
 class WriteCommand {
     constructor() {
+        this.BotConsole = new BotConsole();
     }
 
-    commandGuild(guild) {
-        return new Promise((resolve, reject) => {
+    async commandGuild(guild) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                await this.commandDeleteGuild(guild).then(() => {
 
-            this.commandDeleteGuild(guild).then(() => {
+                    new BotConsole().log("Scrittura iniziata in " + guild.name, "yellow");
 
-                new BotConsole().log("Scrittura iniziata in " + guild.name, "yellow");
+                    const promises = [];
 
-
-
-                if (client.commandg.size === 0) {
-                    new BotConsole().log("Err: Non ci sono comandi da registrare", "red");
-                    return reject(-1);
-                }
-
-                let ne = 0;
-                const promises = [];
-
-
-
-
-                client.commandg.forEach(command => {
-                    if (command.data) {
-                        const commandPromise = guild.commands.create(command.data)
-                            .catch((err) => {
-                                new BotConsole().log("Errore: non ho potuto registrare il comando:" + command.name, "red");
-                            });
-                        promises.push(commandPromise);
-                    } else {
-                        new BotConsole().log("Errore: non ho trovato la sezione data nel comando: " + command.name, "red");
-                        ne++;
-                    }
-                });
-
-                Promise.all(promises)
-                    .then(() => {
-                        if (ne === client.commandg.size) {
-                            new BotConsole().log("Nessun comando registrato correttamente in " + guild.name, "red");
-                            return reject(-1);
+                    client.commandg.forEach(command => {
+                        if (command.data) {
+                            const commandPromise = guild.commands.create(command.data)
+                                .catch(err => {
+                                    console.error(err);
+                                    this.BotConsole.log("Errore: non ho potuto registrare il comando: " + command.name, "red");
+                                });
+                            promises.push(commandPromise);
+                        } else {
+                            this.BotConsole.log("Errore: non ho trovato la sezione data nel comando: " + command.name, "red");
                         }
-                        new BotConsole().log("Scrittura dei comandi avvenuta correttamente in " + guild.name, "green");
-                        resolve(0);
-                    })
-                    .catch(() => {
-                        new BotConsole().log("Err: Impossibile completare la scrittura dei comandi in " + guild.name, "red");
+                    });
+
+                    Promise.allSettled(promises).then(results => {
+                        const successfulCount = results.filter(result => result.status === "fulfilled").length;
+
+                        if (successfulCount === client.commandg.size) {
+                            this.BotConsole.log("Scrittura dei comandi avvenuta correttamente in " + guild.name, "green");
+                            resolve(0);
+                        } else if (successfulCount > 0) {
+                            this.BotConsole.log("Scrittura dei comandi avvenuta con errori in " + guild.name, "yellow");
+                            resolve(0);
+                        } else {
+                            this.BotConsole.log("Errore: non ho potuto scrivere i comandi in " + guild.name, "red");
+                            reject(-1);
+                        }
+                    }).catch(err => {
+                        console.error(err);
+                        this.BotConsole.log("Errore durante la scrittura dei comandi in " + guild.name, "red");
                         reject(-1);
                     });
-            }).catch(() => { reject(-1) })
+
+                }).catch(() => { reject(-1); });
 
 
+            } catch (err) {
+                this.BotConsole.log("Errore nella cancellazione dei comandi in " + guild.name, "red");
+                reject(-1);
+            }
         });
     }
 
-    commandDeleteGuild(guild) {
-
+    async commandDeleteGuild(guild) {
         return new Promise(async (resolve, reject) => {
             try {
-                new BotConsole().log('Cancellazione dei comandi avviata in ' + guild.name, "yellow");
-                const promises = [];
+                this.BotConsole.log("Cancellazione dei comandi in " + guild.name, "yellow");
+
                 let commands = await guild.commands.fetch();
-                if (commands.size != 0) {
-                    for (let command of commands.values()) {
-                        promises.push(command.delete());
-                    }
-                    Promise.all(promises).then(() => {
-                        new BotConsole().log('Slash command cancellati in ' + guild.name, "green");
-                        resolve(0)
-                    }).catch(() => { })
-                } else {
-                    new BotConsole().log("Errore non sono presenti slash command da cancellare in " + guild.name, "red")
-                    resolve(0)
+
+                if (commands.size === 0) {
+                    resolve(0);
+                    return;
                 }
 
-            } catch (err) {
-                new BotConsole().log("Errore Impossibile cancellare gli slash command", "red")
-                reject(-1)
-            }
-        })
+                let deletePromises = [];
 
+                commands.forEach(command => {
+                    const deletePromise = command.delete().catch(() => {
+                        this.BotConsole.log(`Errore nella cancellazione del comando ${command.name} in ${guild.name}`, "red");
+                    });
+                    deletePromises.push(deletePromise);
+                });
+
+                Promise.allSettled(deletePromises).then(results => {
+                    const successfulDeletes = results.filter(result => result.status === "fulfilled").length;
+
+                    if (successfulDeletes === commands.size) {
+                        this.BotConsole.log(`Cancellazione dei comandi completata in ${guild.name}`, "green");
+                        resolve(0);
+                    } else if (successfulDeletes > 0) {
+                        this.BotConsole.log(`Cancellazione dei comandi completata con errori in ${guild.name}`, "yellow");
+                        resolve(-1);
+                    } else {
+                        this.BotConsole.log(`Errore: non ho potuto cancellare i comandi in ${guild.name}`, "red");
+                        reject(-1);
+                    }
+                }).catch(() => {
+                    this.BotConsole.log(`Errore durante la cancellazione dei comandi in ${guild.name}`, "red");
+                    reject(-1);
+                });
+
+            } catch (err) {
+                this.BotConsole.log(err);
+                reject(-1);
+            }
+        });
     }
 
     commandallguild() {
         return new Promise((resolve, reject) => {
-            let promise = []
 
-            client.guilds.cache.forEach(async (guild) => {
-                await promise.push(this.commandGuild(guild).catch(() => { }))
+            let promises = [];
+
+            client.guilds.cache.forEach(guild => {
+                const promise = this.commandGuild(guild).catch(() => {
+                    this.BotConsole.log(`Errore nella registrazione dei comandi per la gilda ${guild.name}`, "red");
+                });
+                promises.push(promise);
             });
-            Promise.all(promise).then(() => {
-                resolve(0)
+
+            Promise.allSettled(promises).then(() => {
+                const registerguild = results.filter(result => result.status === "fulfilled").length;
+
+                if (registerguild === client.guilds.cache.size) {
+                    this.BotConsole.log("Scrittura dei comandi in tutte le gilde completata", "green");
+                    resolve(0);
+                } else if (registerguild > 0) {
+                    this.BotConsole.log("Scrittura dei comandi in tutte le gilde completata con errori", "yellow");
+                    reject(-1);
+                } else {
+                    this.BotConsole.log("Errore: non ho potuto scrivere i comandi in tutte le gilde", "red");
+                    reject(-1);
+                }
             }).catch(() => {
-                reject(-1)
-            })
+                this.BotConsole.log("Errore durante la scrittura dei comandi nelle gilde", "red");
+                reject(-1);
+            });
 
-
-        })
-
-
+        });
     }
 
 
-    commandAllguildonstartup() {
-        return new Promise((resolve) => {
+    async commandAllguildonstartup() {
+        return new Promise(async (resolve) => {
             client.commandg = new Collection([
                 ...client.basecommands,
                 ...client.distubecommands,
-            ])
+            ]);
 
+            let promises = [];
 
-            let countguild = 0;
-            let counterr = 0;
-            let ncommand = client.commandg.size;
-            client.guilds.cache.forEach(async (guild) => {
-                let count = 0;
+            for (let guild of client.guilds.cache.values()) {
+                let commandchange = false;
                 let commands = await guild.commands.fetch();
-                for (let i = 0; i < ncommand; i++) {
-                    if (commands.find(x => x.name === client.commandg.at(i).name)) {
-                        count++;
-                    } else break;
+
+                if (commands.size === client.commandg.size) {
+                    client.commandg.some(command => {
+                        if (commands.find(x => x.data === command.data)) {
+                            commandchange = true;
+                            return true;
+                        }
+                    });
+                } else {
+                    commandchange = true;
                 }
 
-                if (count != ncommand) {
-                    this.commandDeleteGuild(guild).then(() => {
-                        this.commandOneGuild(guild).then(() => { countguild++ }).catch(() => { counterr++ })
-                    }).catch(() => { })
+                if (commandchange) {
+                    promises.push(this.commandGuild(guild));
+                } else {
+                    this.BotConsole.log(`Nessun comando da registrare in ${guild.name}`, "yellow");
                 }
-            })
-
-            if (countguild === client.guilds.cache.size) {
-                new BotConsole().log("Comandi inizializzati correttamente in tutti i server", "green")
-                resolve(0)
-            } if (countguild >= 0) {
-                new BotConsole().log("Comandi inizializzati correttamente in " + countguild + " server", "green")
-                resolve(0)
-            } else {
-                new BotConsole().log("Non devo inizializzare nessun comando", "yellow");
-                resolve(0)
             }
 
-        })
+            if (promises.length === 0) {
+                this.BotConsole.log("Nessun comando da registrare in tutte le gilde", "yellow");
+                resolve(0);
+            } else {
+                Promise.allSettled(promises).then(results => {
+                    const fulfilledCount = results.filter(result => result.status === "fulfilled").length;
+
+                    if (fulfilledCount === promises.length) {
+                        this.BotConsole.log("Scrittura dei comandi in tutte le gilde completata", "green");
+                        resolve(0);
+                    } else if (fulfilledCount > 0) {
+                        this.BotConsole.log("Scrittura dei comandi in tutte le gilde completata con errori", "yellow");
+                        resolve(0);
+                    } else {
+                        this.BotConsole.log("Errore: non ho potuto scrivere i comandi in tutte le gilde", "red");
+                        reject(0);
+                    }
+                    
+                });
+            }
+        });
     }
 
-    async commandOneGuild(guild) {
-        await this.commandGuild(guild).catch(() => { })
-    }
 }
 
 module.exports = {
