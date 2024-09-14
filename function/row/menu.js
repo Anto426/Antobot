7e2a6b9988ebba5412a19f55567228c9095d30bc
Anto426 <amanocchio426@gmail.com>
2024-09-14 19:17:59 +0200

    Refactor: Update createMenu method in menu.js

--- function/row/menu.js
+++ function/row/menu.js
@@ -5,52 +5,66 @@ class Menu {
 
     }
 
-    createMenu(list, id, field, idUser, npage) {
+    createMenu(list, listselection, id, field, idUser, npage, type) {
+
+
 
         let row = new ActionRowBuilder();
         let row0 = new ActionRowBuilder();
 
         let incremento = 25 * npage;
+        let listtotsize = list.length + listselection.length;
         let components = [];
 
         let tlist = list;
 
         let bup = new ButtonBuilder()
-            .setCustomId(`${id}-${idUser}-up-0`)
+            .setCustomId(`${id}-${idUser}-${parseInt(npage) + 1}-${type}`)
             .setLabel('Avanti')
             .setStyle(ButtonStyle.Success)
             .setDisabled(true);
 
         let bdown = new ButtonBuilder()
-            .setCustomId(`${id}-${idUser}-down-0`)
+            .setCustomId(`${id}-${idUser}-${parseInt(npage) - 1}-${type}`)
             .setLabel('Indietro')
             .setStyle(ButtonStyle.Success)
             .setDisabled(true);
 
 
-        if (list.length >= 25) {
+        field.setCustomId(`${id}-${idUser}-${npage}-${type}`)
 
-            tlist = list.slice(0 + incremento, 25 + incremento);
-            if ((list.length / 25) < npage) {
+
+        if (listtotsize >= 25) {
+
+
+            list.slice(0 + incremento - listselection.length, 25 + incremento - listselection.length).forEach(element => {
+                listselection.push(element);
+            });
+
+            tlist = listselection
+
+            if (((listtotsize - listtotsize % 25) / 25) > npage) {
                 bup.setDisabled(false);
             }
+
             if (npage != 0) {
                 bdown.setDisabled(false);
             }
         }
+
 
         tlist.forEach(element => {
             field.addOptions(element);
         });
 
         row.addComponents(field);
-        row0.addComponents(bup, bdown);
+        row0.addComponents(bdown, bup);
 
         components.push(row, row0);
 
         return components;
-    }
 
+    }
 
 
 }
