b73f045c4173b065542f78462c90af19fc746edc
Anto426 <amanocchio426@gmail.com>
2024-09-19 00:00:32 +0200

    Refactor: Update boostevent.js to fix bug in premium member event handling

--- base/events/Guild/boostevent.js
+++ base/events/Guild/boostevent.js
@@ -7,21 +7,21 @@ module.exports = {
     allowevents: true,
     async execute(oldMember, newMember) {
         if (oldMember.premiumSince === newMember.premiumSince) return
-            let embedmsg = new EventEmbed(oldMember.guild, oldMember);
-            let json = new Cjson();
-            embedmsg.init().then(() => {
-                json.readJson(process.env.dirdatabase + setting.database.root + "/" + setting.database.guildconfig).then((data) => {
-                    if (data[newMember.guild.id]) {
-                        let channel = newMember.guild.channels.cache.find(x => x.id === data[newMember.guild.id].channel.boost)
-                        channel.send({ embeds: [embedmsg.boostEvent(newMember)] })
-                    }
-                }).catch((err) => {
-                    console.log(err);
-                })
+        let embedmsg = new EventEmbed(oldMember.guild, oldMember);
+        let json = new Cjson();
+        embedmsg.init().then(() => {
+            json.readJson(process.env.dirdatabase + setting.database.root + "/" + setting.database.guildconfig).then((data) => {
+                if (data[newMember.guild.id]) {
+                    let channel = newMember.guild.channels.cache.find(x => x.id === data[newMember.guild.id].channel.boost)
+                    channel.send({ embeds: [embedmsg.boostEvent(newMember)] })
+                }
             }).catch((err) => {
                 console.log(err);
             })
-        
+        }).catch((err) => {
+            console.log(err);
+        })
+
 
     }
 }
