c8363be29e1402d2ae96d16b5da0c632658b3080
Anto426 <amanocchio426@gmail.com>
2024-09-18 23:07:45 +0200

    Refactor: Update loadothermodule.js to improve module loading logic

--- function/loadothermodule/loadothermodule.js
+++ function/loadothermodule/loadothermodule.js
@@ -5,15 +5,17 @@ const { Status } = require("../status/status");
 
 class Loadothermodules {
     constructor() {
-        this.check = new Check()
-        this.status = new Status()
-        this.holiday = new Holiday()
         this.BotConsole = new BotConsole()
+        this.check = new Check()
     }
 
     load() {
-        this.check.checkAllowStatus().then(() => { this.BotConsole.log("Modulo status caricato", "green"); this.status.updateStatus(); this.status.updateStatusEveryFiveMinutes() }).catch(() => { this.BotConsole.log("Modulo status non caricato", "red") })
-        this.check.checkAllowHoliday().then(() => { this.BotConsole.log("Modulo hollyday caricato", "green"); this.holiday.init().then(() => { this.holiday.main() }).catch(() => { }); }).catch((err) => { console.log(err); this.BotConsole.log("Modulo hollyday non caricato", "red") })
+        
+        client.holidaymodule = new Holiday()
+        client.statusmodule = new Status()
+
+        this.check.checkAllowStatus().then(() => { this.BotConsole.log("Modulo status caricato", "green"); client.statusmodule.updateStatus(); client.statusmodule.updateStatusEveryFiveMinutes() }).catch(() => { this.BotConsole.log("Modulo status non caricato", "red") })
+        this.check.checkAllowHoliday().then(() => { this.BotConsole.log("Modulo hollyday caricato", "green"); client.holidaymodule.init().then(() => { client.holidaymodule.main() }).catch(() => { }); }).catch((err) => { console.log(err); this.BotConsole.log("Modulo hollyday non caricato", "red") })
     }
 
 }
