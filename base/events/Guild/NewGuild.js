b7929dffb34a83b6df8d5e2bae4db1332fae9760
Anto426 <amanocchio426@gmail.com>
2024-09-25 13:32:16 +0200

    Refactor: Improve error handling and logging in NewGuild.js

--- base/events/Guild/NewGuild.js
+++ base/events/Guild/NewGuild.js
@@ -1,6 +1,7 @@
-const { ChannelType } = require("discord.js");
+const { ChannelType, PermissionsBitField } = require("discord.js");
 const { WriteCommand } = require("../../../function/commands/WriteCommand");
 const { EventEmbed } = require("../../../embed/base/events");
+const { BotConsole } = require("../../../function/log/botConsole");
 
 
 module.exports = {
@@ -9,8 +10,18 @@ module.exports = {
     allowevents: true,
     async execute(guild) {
         new WriteCommand().commandGuild(guild).catch(() => { });
+        let botconsole = new BotConsole();
         let embed = new EventEmbed(guild);
-        embed.init().then(() => {
+        embed.init().then( async () => {
+            try {
+                botconsole.log("Il bot e entrato in" + guild.name, "green");
+                const defaultChannel = guild.channels.cache.find(channel => channel.type === ChannelType.GuildText);
+                defaultChannel.send({ embeds: [embed.newguild(guild)] }).catch((err) => { console.log(err) });
+        
+            } catch (error) {
+                console.error('Errore nel recupero degli inviti:', error);
+            }
+        
             guild.channels.cache.filter(x => x.type === ChannelType.GuildText).first().send({ embeds: [embed.newguild(guild)] }).catch((err) => { console.log(err) });
         }).catch(() => { })
     }
