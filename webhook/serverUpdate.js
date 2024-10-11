0e140eed9300257ab5ce923683d2a434aa48cbb4
Anto426 <amanocchio426@gmail.com>
2024-10-11 16:35:57 +0200

    Refactor: Add UpdateReceived method to serverUpdate.js

--- webhook/serverUpdate.js
+++ webhook/serverUpdate.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const { BotConsole } = require("../function/log/botConsole");
+const { log } = require('../function/log/log');
 
 
 class serverUpdate {
 
     constructor() {
+        this.log = new log();
         this.botconsole = new BotConsole();
         this.app = new express();
         this.app.use(express.json());
@@ -17,9 +19,11 @@ class serverUpdate {
         return new Promise((resolve, reject) => {
             try {
                 this.app.post('/webhook', (req, res) => {
-                    console.log('Webhook received: '+ req.body);
                     res.status(200).send('Webhook processed');
-                  });
+                    this.log.init().then(() => {
+                        this.log.UpdateRecived(req.body.head_commit);
+                    }).catch(() => { console.log("Errore nell'inizializzare il modulo log") });
+                });
                 resolve(0);
             } catch (error) {
                 console.log(error);
