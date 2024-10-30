22cfc57f3fbaff0568afb364eb263e74637b1daa
Anto426 <amanocchio426@gmail.com>
2024-10-30 15:25:37 +0100

    remove unusual code 🫤

--- distube/events/error.js
+++ distube/events/error.js
@@ -6,27 +6,13 @@ module.exports = {
     typeEvent: "error",
     allowevents: true,
     async execute(error, queue, song) {
-        try {
-            console.log(error);
-            let embedmsg = new EventEmbed(queue.textChannel.guild);
-            embedmsg.init().then(() => {
-                queue.textChannel.send({ embeds: [embedmsg.error(song)], ephemeral: true });
-            }).catch((err) => {
-                console.error(err);
-                let embedmsg = new ErrEmbed(queue.textChannel.guild);
-                embedmsg.init().then(() => {
-                    interaction.reply({ embeds: [embedmsg.genericError()], ephemeral: true }).catch((err) => {
-                        console.error(err);
-                    });
-                }).catch((err) => {
-                    console.error(err);
-                });
-
-            });
-        } catch (err) {
+        console.log(error);
+        let embedmsg = new EventEmbed(queue.textChannel.guild);
+        embedmsg.init().then(() => {
+            queue.textChannel.send({ embeds: [embedmsg.error(song)], ephemeral: true });
+        }).catch((err) => {
             console.error(err);
-        }
-
+        });
     }
 }
 
--- distube/events/play.js
+++ distube/events/play.js
@@ -1,6 +1,4 @@
 const { EventEmbed } = require("../../embed/distube/events");
-const { ErrEmbed } = require("../../embed/err/errembed");
-
 module.exports = {
     name: "Play",
     typeEvent: "playSong",
@@ -10,14 +8,6 @@ module.exports = {
         embedmsg.init().then(() => {
             queue.textChannel.send({ embeds: [embedmsg.trackStart(queue, song)] })
         }).catch((err) => {
-            let embedmsg = new ErrEmbed(interaction.guild, interaction.member);
-            embedmsg.init().then(() => {
-                interaction.reply({ embeds: [embedmsg.genericError()], ephemeral: true }).catch((err) => {
-                    console.error(err);
-                });
-            }).catch((err) => {
-                console.error(err);
-            });
             console.log(err);
         });
     }
