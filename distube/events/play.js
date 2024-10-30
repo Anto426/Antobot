343730714281735e355b81debe73a498039d18e3
Anto426 <amanocchio426@gmail.com>
2024-10-30 20:55:50 +0100

    Refactor: Enhance play event with dynamic color extraction for improved embed visuals

--- distube/events/play.js
+++ distube/events/play.js
@@ -1,12 +1,22 @@
 const { EventEmbed } = require("../../embed/distube/events");
+const { DynamicColor } = require("../../function/Color/DynamicColor");
 module.exports = {
     name: "Play",
     typeEvent: "playSong",
     allowevents: true,
     async execute(queue, song) {
         const embedmsg = new EventEmbed(queue.textChannel.guild);
+        let dynamiccolor = new DynamicColor();
+        await dynamiccolor.setImgUrl(song.thumbnail).catch((err) => { console.error(err) });
+        dynamiccolor.setNumcolorextract(2);
+        dynamiccolor.setThreshold(50);
+
         embedmsg.init().then(() => {
-            queue.textChannel.send({ embeds: [embedmsg.trackStart(queue, song)] })
+            dynamiccolor.ExtractPalet().then((pallet) => {
+                queue.textChannel.send({ embeds: [embedmsg.trackStart(queue, song, dynamiccolor.ColorFunctions.rgbToHex(pallet[0][0], pallet[0][1], pallet[0][2]))] })
+            }).catch((err) => {
+                console.error(err);
+            });
         }).catch((err) => {
             console.log(err);
         });
