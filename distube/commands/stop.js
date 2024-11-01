c60d98468c406d7141cec4c9d374fab2e8d56fac
Anto426 <amanocchio426@gmail.com>
2024-11-01 14:06:49 +0100

    fix: Add 'exit_on_channel' option to stop command for voice channel exit

--- distube/commands/stop.js
+++ distube/commands/stop.js
@@ -16,13 +16,25 @@ module.exports = {
     data: {
         name: "stop",
         description: "pulisci la coda",
+        options: [
+            {
+                name: "exit_on_channel",
+                description: "esci dal canale vocale",
+                type: 5,
+                required: false
+            }
+        ]
     },
     async execute(interaction) {
 
         return new Promise((resolve, reject) => {
             let embedmsg = new CommandEmbed(interaction.guild, interaction.member)
+            let exit = interaction.options.getBoolean("exit_on_channel") || false
             embedmsg.init().then(() => {
                 distube.stop(interaction).then(() => {
+                    console.log(exit)
+                    if (exit)
+                        distube.voices.leave(interaction.guild.id)
                     interaction.reply({ embeds: [embedmsg.stop()] }).catch((err) => {
                         console.error(err);
                     })
