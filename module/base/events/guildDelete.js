1940805a20f08e344ce0395cc51473d308599ff6
Anto426 <amanocchio426@gmail.com>
2025-06-22 16:17:13 +0200

    refactor: enhance code formatting and readability in guildDelete event handler

--- module/base/events/guildDelete.js
+++ module/base/events/guildDelete.js
@@ -12,29 +12,43 @@ export default {
         ID: guild.id,
       });
 
-      BotConsole.info(`Interruzione dei processi in memoria per la gilda: ${guild.name}`);
-      
-      const holidayModule = guild.client.modules?.get('Holiday');
+      BotConsole.info(
+        `Interruzione dei processi in memoria per la gilda: ${guild.name}`
+      );
+
+      const holidayModule = guild.client.modules?.get("Holiday");
 
       if (holidayModule) {
         holidayModule.stopGuildTracking(guild.id);
-        BotConsole.info(`Processo "Holiday" per la gilda ${guild.name} interrotto.`);
+        BotConsole.info(
+          `Processo "Holiday" per la gilda ${guild.name} interrotto.`
+        );
       } else {
-        BotConsole.warning(`Modulo "Holiday" non trovato o non inizializzato, pulizia memoria saltata.`);
+        BotConsole.warning(
+          `Modulo "Holiday" non trovato o non inizializzato, pulizia memoria saltata.`
+        );
       }
-      
-      BotConsole.info(`Inizio rimozione dati dal database per la gilda con ID: ${guild.id}...`);
+
+      BotConsole.info(
+        `Inizio rimozione dati dal database per la gilda con ID: ${guild.id}...`
+      );
 
       const deleteResult = await SqlManager.deleteGuild(guild.id);
-      
-      if (deleteResult.affectedRows > 0) {
-        BotConsole.success(`Dati per la gilda "${guild.name}" e info collegate rimossi con successo dal DB.`);
-      } else {
-        BotConsole.info(`Nessun dato da rimuovere per la gilda "${guild.name}".`);
-      }
 
+      if (deleteResult.affectedRows > 0) {
+        BotConsole.success(
+          `Dati per la gilda "${guild.name}" e info collegate rimossi con successo dal DB.`
+        );
+      } else {
+        BotConsole.info(
+          `Nessun dato da rimuovere per la gilda "${guild.name}".`
+        );
+      }
     } catch (error) {
-      BotConsole.error(`Errore durante l'evento guildDelete per la gilda con ID: ${guild.id}.`, error);
+      BotConsole.error(
+        `Errore durante l'evento guildDelete per la gilda con ID: ${guild.id}.`,
+        error
+      );
     }
   },
 };
