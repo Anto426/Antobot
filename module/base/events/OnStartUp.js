e994daf72b461d4f7a8ac4a76c62aead07e332d7
Anto426 <amanocchio426@gmail.com>
2025-07-25 17:32:32 +0200

    feat: add SQL connection setup during bot startup

--- module/base/events/OnStartUp.js
+++ module/base/events/OnStartUp.js
@@ -3,6 +3,8 @@ import ModuleLoader from "../../../class/Loader/ModuleLoader.js";
 import StartupLogger from "../../../class/console/LogStartup.js";
 import CommandGuildUpdate from "../../../class/Guild/CommandGuildUpdate.js";
 import IntitialOtherModules from "../../../class/Loader/IntitialOtherModules.js";
+import SqlManager from "../../../class/services/SqlManager.js";
+import ConfigManager from "../../../class/services/ConfigManager.js";
 export default {
   name: "OnStartUp",
   eventType: "ready",
@@ -13,6 +15,11 @@ export default {
     await CommandGuildUpdate.updateGuildsOnStartup();
     BotConsole.success("Tutte le gilde sono state aggiornate con i comandi.");
     await IntitialOtherModules.Init();
+    const sqlConfig = ConfigManager.getConfig("sql");
+    await SqlManager.connect(sqlConfig);
+    BotConsole.info(
+      `[OnStartUp] Connessione al database SQL stabilita con successo.`
+    );
     BotConsole.success("Tutti i moduli secondari sono stati inizializzati.");
     await StartupLogger.run();
     client.botready = true;
